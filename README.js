3dc8a5a5265ecb8f2b126c5c155b16cf52e2484a
Matheus Vieira <68302945+Vieiravs2@users.noreply.github.com>
2022-04-29 11:22:46 -0300

    Update README.js

--- README.js
+++ README.js
@@ -1,10 +1,1 @@
 
-let vieira = {
-   age: 19,
-   coutry: 'Brasil',
-   state: 'São Paulo',
-   city: 'Ribeirão Preto',
-   actualStack: ['LUA', 'MySql', 'MongoDB', 'Javascript', 'Node.JS', 'React JS', 'React Native', 'CSS3', 'HTML5'],
-   hobbies: ['Love Programming and gaming']
-}
-      
